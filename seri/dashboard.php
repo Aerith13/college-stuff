645ed7db40165a596c4492a86d72689acab31841
昀昀 <35630143+Aerith13@users.noreply.github.com>
2023-04-13 16:03:03 +0800

    Add files via upload

---
+++ seri/dashboard.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+
+WELCOME <?php echo $_POST["fname"];echo $_POST["middle"];echo $_POST["lname"]; ?><br>
+from: <?php echo $_POST["address"]; ?><br>
+Gender of: <?php echo $_POST["gender"]; ?><br>
+Age is: <?php echo $_POST["age"]; ?><br>
+About Me: <?php echo $_POST["about"]; ?><br>
+
+<html>
+<body>
